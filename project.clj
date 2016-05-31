d8568fd571579beba102a6eb92d18465e9ecf483
Dominic <dominic@juxt.pro>
2016-05-31 16:41:30 +0100

    Bump up dependencies

--- project.clj
+++ project.clj
@@ -1,5 +1,8 @@
-(defproject snowballer "0.1.0-SNAPSHOT"
-  :description "A bukkure plugin to..."
+(defproject snowballer "0.2.0+alpha"
+  :description "A bukkure plugin to throw snowballs with sticks"
+  :repositories [["spigot-repo" "https://hub.spigotmc.org/nexus/content/groups/public/"]]
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [bukkure "0.1.0-SNAPSHOT"]]
-  :java-source-paths ["javasrc"])
+                 [bukkure "0.4.3"]]
+  :java-source-paths ["javasrc"]
+  :profiles {:provided
+             {:dependencies [[org.bukkit/bukkit "1.8.8-R0.1-SNAPSHOT"]]}})
