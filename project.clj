bcccb6e544ba5e8e8b6f27517b9a07e0b884ae76
Dominic <dominic@juxt.pro>
2016-05-31 21:20:33 +0100

    Update to 1.9.4

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject snowballer "0.2.0+alpha"
+(defproject snowballer "0.3+alpha"
   :description "A bukkure plugin to throw snowballs with sticks"
   :repositories [["spigot-repo" "https://hub.spigotmc.org/nexus/content/groups/public/"]]
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [bukkure "0.4.3"]]
+                 [bukkure "1.6+alpha"]]
   :java-source-paths ["javasrc"]
   :profiles {:provided
-             {:dependencies [[org.bukkit/bukkit "1.8.8-R0.1-SNAPSHOT"]]}})
+             {:dependencies [[org.bukkit/bukkit "1.9.4-R0.1-SNAPSHOT"]]}})
